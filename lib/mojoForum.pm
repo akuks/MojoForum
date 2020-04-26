3d5e9bf8a97ba434e9929b623e270a0f3cf839ec
ashutosh <kukreti.ashutosh@gmail.com>
2020-04-26 14:21:23 +0530

    Initial Commit

---
+++ lib/mojoForum.pm
@@ -0,0 +1,21 @@
+package mojoForum;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Load configuration from hash returned by config file
+  my $config = $self->plugin('Config');
+
+  # Configure the application
+  $self->secrets($config->{secrets});
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('example#welcome');
+}
+
+1;
