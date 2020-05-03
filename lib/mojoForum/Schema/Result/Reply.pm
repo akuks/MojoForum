ec3d0e30271487e8fa6ef5c4a0794356f9dabfb2
ashutosh <kukreti.ashutosh@gmail.com>
2020-05-03 17:56:36 +0530

    Reply Schema Added

---
+++ lib/mojoForum/Schema/Result/Reply.pm
@@ -0,0 +1,156 @@
+use utf8;
+package mojoForum::Schema::Result::Reply;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+mojoForum::Schema::Result::Reply
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::TimeStamp>
+
+=item * L<DBIx::Class::EncodedColumn>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "EncodedColumn");
+
+=head1 TABLE: C<replies>
+
+=cut
+
+__PACKAGE__->table("replies");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 user_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 1
+
+=head2 thread_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 1
+
+=head2 body
+
+  data_type: 'mediumtext'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "user_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 1,
+  },
+  "thread_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 1,
+  },
+  "body",
+  { data_type => "mediumtext", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 thread
+
+Type: belongs_to
+
+Related object: L<mojoForum::Schema::Result::Thread>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "thread",
+  "mojoForum::Schema::Result::Thread",
+  { id => "thread_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 user
+
+Type: belongs_to
+
+Related object: L<mojoForum::Schema::Result::User>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "user",
+  "mojoForum::Schema::Result::User",
+  { id => "user_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2020-04-28 21:43:28
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IOZrJ9lFX6fZTq+1kZSYKQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
