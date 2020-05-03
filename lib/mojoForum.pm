bd75f4514c84677ce198eb6f26506124ef813285
ashutosh <kukreti.ashutosh@gmail.com>
2020-05-03 17:55:04 +0530

    Add Thread route

--- lib/mojoForum.pm
+++ lib/mojoForum.pm
@@ -1,21 +1,33 @@
 package mojoForum;
 use Mojo::Base 'Mojolicious';
 
+use mojoForum::Model::DB;
+
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Load configuration from hash returned by config file
-  my $config = $self->plugin('Config');
+    # Load configuration from hash returned by config file
+    my $config = $self->plugin('Config');
 
-  # Configure the application
-  $self->secrets($config->{secrets});
+    # Configure the application
+    $self->secrets( $config->{secrets} );
 
-  # Router
-  my $r = $self->routes;
+    $self->_db_handler();
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    # Router
+    my $r = $self->routes;
+
+    # Normal route to controller
+    $r->get('/')->to(controller => 'Thread', action => 'show');
+}
+
+sub _db_handler {
+    my $self = shift;
+
+    $self->{dbh} = mojoForum::Model::DB->new();
+
+    return $self;
 }
 
 1;
