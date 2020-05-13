9446d037bef320adc9697425e0c5c2fe531e1e77
ashutosh <kukreti.ashutosh@gmail.com>
2020-05-14 01:21:14 +0530

    Modify thread controller

--- lib/mojoForum/Controller/Thread.pm
+++ lib/mojoForum/Controller/Thread.pm
@@ -6,19 +6,24 @@ sub show {
     my $self = shift;
 
     my $dbh = $self->app->{dbh};			# Database handler
-    
+    my $paginate = $self->app->{paginate};	# paginate
+
+    my $total_threads = $dbh->resultset('Thread')->search({})->count;
+
     # Fetch all the threads from the thread table;
-    my @threads = $dbh->resultset('Thread')->search({});
+    my @threads = $dbh->resultset('Thread')->search({}, { rows => $paginate });
     
     @threads = map { { 
         title => $_->title,
         body  => $_->body,
-        user  => $_->user->first_name. ' ' . $_->user->last_name
+        user  => $_->user->full_name,
+        id    => $_->id
     } } @threads;
-
+    
     $self->render(
         template => 'thread',				# Template name, thread.html.ep under the templates folder.
-        threads  => \@threads				# Pass thread array ref to the template
+        threads  => \@threads,				# Pass thread array ref to the template
+        total_pages => $total_threads / $paginate
     );
 }
 
